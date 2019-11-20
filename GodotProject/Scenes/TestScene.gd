4279306da8c91099b5e42eb394a380593ac94be5
Dylan Santiago <50032722+WayOfOld@users.noreply.github.com>
2019-11-20 07:29:45 -0500

    Level

--- GodotProject/Scenes/TestScene.gd
+++ GodotProject/Scenes/TestScene.gd
@@ -2,7 +2,23 @@ extends Node2D
 
 export (PackedScene) var Arrow
 
+var resources = {
+	"Wood" : 0,
+	"Iron" : 0,
+	"String": 0,
+	"Stone" : 0,
+	"Crystal" : 0,
+	"Life Essence" : 0,
+	"Mana Essence" : 0,
+	"Damage Essence" : 0,
+	"Bottle" : 0
+	}
+
 func _on_NeutralHood_shoot(pos, dir):
 	var a = Arrow.instance()
 	a.start(pos, deg2rad(dir))
 	add_child(a)
+
+
+func _on_WoodCollectible_body_entered(body):
+	print(body)
