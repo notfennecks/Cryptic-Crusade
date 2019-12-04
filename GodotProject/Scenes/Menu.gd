feee6ef07a099d9a3454076489f5421b7a223ec6
John <johnmelo564@gmail.com>
2019-12-03 23:35:29 -0500

    Menu Added

---
+++ GodotProject/Scenes/Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_StartButton_button_up():
+	$Menu/Center/Buttons/Timer.start()
+
+func _on_StartButton_button_down():
+	$Faded.show()
+	$Faded/AnimationPlayer.play("Fading")
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://Scenes/TestScene.tscn")
