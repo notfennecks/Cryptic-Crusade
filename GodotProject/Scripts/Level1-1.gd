68c8a487afeec777bb8ed0d7d0f85f20cde3596d
BrownKing <brownkevon616@gmail.com>
2019-12-04 22:46:22 -0500

    Set up level 1-1

---
+++ GodotProject/Scripts/Level1-1.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+onready var bar = $ProgressBar
+onready var character = $Player
+export (PackedScene) var Arrow
+
+func _ready():
+#	bar._init(character.experience_required, character.experience_total)
+	pass
+	
+func _process(delta):
+	if Input.is_action_just_pressed("inventory"):
+		if $CanvasLayer/InventoryScreen.visible:
+			$CanvasLayer/InventoryScreen.hide()
+		else:
+			$CanvasLayer/InventoryScreen.show()
+	if Input.is_action_just_pressed("crafting"):
+		if $CanvasLayer/CraftingScreen.visible:
+			$CanvasLayer/CraftingScreen.hide()
+		else:
+			$CanvasLayer/CraftingScreen.show()
+
+func _on_Player_shoot(pos, dir):
+	var a = Arrow.instance()
+	a.start(pos, deg2rad(dir))
+	add_child(a)
