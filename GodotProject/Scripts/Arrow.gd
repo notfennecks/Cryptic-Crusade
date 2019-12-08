db01d41644e95ac6a658c346bb6d4e14836773aa
Dylan Santiago <50032722+WayOfOld@users.noreply.github.com>
2019-12-08 15:37:45 -0500

    Arrow and environment update

--- GodotProject/Scripts/Arrow.gd
+++ GodotProject/Scripts/Arrow.gd
@@ -12,11 +12,12 @@ func _process(delta):
 	position += velocity * delta
 
 func _on_Arrow_body_entered(body):
-	if body.name == "Environment" || body.name == "CaveEnvironment":
+	if body.name == "Environment":
 		velocity = Vector2(0, 0)
+		$CollisionShape2D.disabled = true
 		yield(get_tree().create_timer(1), "timeout")
 		queue_free()
-	if body.is_in_group("enemies"):
+	if body.is_in_group("enemies") and $CollisionShape2D.disabled == false:
 		if body.invincible == true:
 			return
 		body.take_damage()
